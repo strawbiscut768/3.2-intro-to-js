7430c335bff00faa17845c51c380c23bad8c9ee4
strawbiscut768 <123778138+strawbiscut768@users.noreply.github.com>
2023-10-26 10:31:18 -0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,4 @@
+console.log("dh;waskjbvs")
+console.log("djgqasdljbvldxfjbdsfaisdwsed sdcivbaksjdbfalkxnbvasdlcsad")
+console.log("=conslole.,vsaldsvwdsnvawldjbvasdfkjbvaosldbv;kalsfdb v;aksdfn")
+alert("fdbg sij;dfgbdskfjhbvlkjdsfnglkdfang;jdfbng;lkjdsfahkajsdhg;lshd'oisdhisdf'sdjfg'askjg;sedjnmg;kdjf nglkdfglskdfnglhksdfghskjfhgsdfbgkdf gjdhf;gbjsd ;gkdhfsgk jdfs;gjsdf;gdf;b jdvnjfgho ;fbndsl;fghdsf;og sd;fobhsd;flkn dsghdfs;kghdsg gdsfokgj'dsl jgk'sdfh'dofkjadnfg wakjg'ofkjsodkjfperjhgeidjgsdojpishasdkhytadgaspdgjasdh;kf")
